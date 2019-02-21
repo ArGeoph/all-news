7162791bfac07f856c25329af1d22700cd4696e5
Kirill Golubev <arnasay@gmail.com>
2019-02-20 22:47:27 -0500

    Now search results are sorted by date. News sources are grouped in categories now

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const techcrunch = document.getElementById('techcrunch');
 const formObject = document.getElementById("sources");
 const sourcesList = document.getElementById('newSources');
 const sourcesMap = new Map();
+let newsCategories = [];
 const sourcesException = []; //variable used to track all news sources that don't contain proper content, but only description
 //Search related HTML objects
 const userSearch = document.getElementById('search');
@@ -61,44 +62,97 @@ const initialize = () => {
 //Function initializing map containing news sources and their urls, and adding them to html list
 const initializeNewsSources = () => {
   //Map used to store urls for new news sources
-  sourcesMap.set("Select news", "");
-  sourcesMap.set("AlJazeera", "al-jazeera-english");
-  sourcesMap.set("Google News Canada", "google-news-ca");
-  sourcesMap.set("Google News Russia", "google-news-ru");
-  sourcesMap.set("New Scientist", "new-scientist");
-  sourcesMap.set("National Geographic", "national-geographic");
-  sourcesMap.set("RBC", "rbc");
-  sourcesMap.set("The Telegraph", "the-telegraph");
-  sourcesMap.set("The Guardian", "the-guardian-uk");
-  sourcesMap.set("The Washington Post", "the-washington-post");
-  sourcesMap.set("Russia Today", "rt");
-  sourcesMap.set("Wired", "wired");
-  sourcesMap.set("The Huffington Post", "the-huffington-post");
-  sourcesMap.set("BBC Sport", "bbc-sport");
-  sourcesMap.set("Next web", "the-next-web");
-  sourcesMap.set("Recode", "recode");
-  sourcesMap.set("Crypto News", "crypto-coins-news")
-  sourcesMap.set("Hacker News", "hacker-news");
-  sourcesMap.set("Buzzfeed", "buzzfeed");
-  sourcesMap.set("Time", "time");
-  sourcesMap.set("Financial Times", "financial-times");
-  sourcesMap.set("Fox News", "fox-news");
-  sourcesMap.set("Independent", "independent");
-  sourcesMap.set("Lenta", "lenta");
-  sourcesMap.set("Reddit /r/all", "reddit-r-all");
-  sourcesMap.set("The Economist", "the-economist");
+  sourcesMap.set("Select news", { category: '',
+                                  url: ''});
+  sourcesMap.set("AlJazeera", { category: 'World News',
+                                url: 'al-jazeera-english'});
+  sourcesMap.set("Google News Canada", { category: "World News",
+                                        url: 'google-news-ca'});
+  sourcesMap.set("Google News Russia", { category: "Russian News",
+                                        url: "google-news-ru"});
+  sourcesMap.set("New Scientist", { category: 'Technologies',
+                                    url: "new-scientist"});
+  sourcesMap.set("National Geographic", { category: 'Entertainment',
+                                          url: "national-geographic"});
+  sourcesMap.set("RBC", { category: "Russian News",
+                          url: "rbc"});
+  sourcesMap.set("The Telegraph", { category: 'World News',
+                                    url: "the-telegraph"});
+  sourcesMap.set("The Guardian", { category: 'World News',
+                                    url: "the-guardian-uk"});
+  sourcesMap.set("The Washington Post", { category: 'World News',
+                                          url: "the-washington-post"});
+  sourcesMap.set("Russia Today", { category: "Russian News",
+                                    url: "rt"});
+  sourcesMap.set("Wired", { category: 'Technologies',
+                            url: "wired"});
+  sourcesMap.set("The Huffington Post", { category: 'World News',
+                                          url: "the-huffington-post"});
+  sourcesMap.set("BBC Sport", { category: "Sport",
+                                url:"bbc-sport"});
+  sourcesMap.set("Next web", { category: 'Technologies',
+                                url: "the-next-web"});
+  sourcesMap.set("Recode", { category: 'Technologies',
+                              url: "recode"});
+  sourcesMap.set("Crypto News", { category: 'Technologies',
+                                  url: "crypto-coins-news"})
+  sourcesMap.set("Hacker News", { category: 'Technologies',
+                                  url: "hacker-news"});
+  sourcesMap.set("Buzzfeed", { category: 'Technologies',
+                                url: "buzzfeed"});
+  sourcesMap.set("Time", { category: 'World News',
+                          url: "time"});
+  sourcesMap.set("Financial Times", { category: 'Business News',
+                                      url:"financial-times"});
+  sourcesMap.set("Fox News", { category: "World News",
+                               url: "fox-news"});
+  sourcesMap.set("Independent", { category: "World News",
+                                  url: "independent"});
+  sourcesMap.set("Lenta", { category: "Russian News",
+                            url:"lenta"});
+  sourcesMap.set("Reddit /r/all",  { category: 'Entertainment',
+                                      url: "reddit-r-all"});
+  sourcesMap.set("The Economist", { category: 'Business News',
+                                    url: "the-economist"});
   sourcesMap.set("USA Today", "usa-today");
-  sourcesMap.set("Ars Technica", "ars-technica");
-  sourcesMap.set("Entertainment Weekly", "entertainment-weekly");
-  sourcesMap.set("Business Insider (UK)", "business-insider-uk");
-  sourcesMap.set("TechBusiness Insider", "business-insider");
+  sourcesMap.set("Ars Technica", { category: 'Technologies',
+                                    url: "ars-technica"});
+  sourcesMap.set("Entertainment Weekly", { category: 'Entertainment',
+                                            url: "entertainment-weekly"});
+  sourcesMap.set("Business Insider", { category: 'Business News',
+                                       url: "business-insider"});
+  //Fill up categories array
+  newsCategories = ['', 'World News', 'Business News', 'Technologies', 'Entertainment', 'Russian News', 'Sport'];
 
-  sourcesMap.forEach((key, value) => {    
-    let newNewsSource = document.createElement("option");
-    newNewsSource.setAttribute("value", key);
-    newNewsSource.innerHTML = value;
-    sourcesList.appendChild(newNewsSource); 
+  //Create and fill up dropdownlist with categories
+  newsCategories.forEach(category => {
+    let categoryObject = document.createElement('optgroup');
+    categoryObject.setAttribute('label', category);
+
+    //Iterate through all news sources and populate news categories with corresponding news sources
+    sourcesMap.forEach((key, value) => { 
+
+      if (key.category === category) {
+        let newNewsSource = document.createElement("option"); 
+        newNewsSource.setAttribute("value", key.url);
+        newNewsSource.innerHTML = value;
+       
+        if (category === '') {
+          sourcesList.appendChild(newNewsSource);
+        }
+        else {
+          categoryObject.appendChild(newNewsSource);
+        }
+      } 
+    }); 
+    
+    //Append category to select HTML object
+    if (category !== '') {
+      sourcesList.appendChild(categoryObject);
+    }
+
   });
+
 };
 
 //News callback function used to load and render news from the url passed as a parameter
@@ -128,7 +182,8 @@ const searchArticles = (userInput) => {
       }  
       else {
         //Print error message if search hasn't returned any results
-        main.innerHTML = "<p class='error'>Your search hasn't returned any results.Please try again later or check your Internet connection</p>";
+        main.innerHTML = `<p class='error'>Your search hasn't returned any results. 
+          Please try again later or check your Internet connection</p>`;
       }    
     });
   }
@@ -139,7 +194,7 @@ const getSearchResults = async (userInput) => {
   try {
     //Clean the page and put spinner element
     main.innerHTML = '<div class="spinner"></div>';
-    const request = await fetch(`https://newsapi.org/v2/everything?sortBy=published&q=${userInput}${apiKey}&pageSize=50`);
+    const request = await fetch(`https://newsapi.org/v2/everything?sortBy=publishedAt&q=${userInput}${apiKey}&pageSize=50`);
     if (request.ok) {
       const requestJson = await request.json();
 
