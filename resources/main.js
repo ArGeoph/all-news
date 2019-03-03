eedf99b486157368af1dff39a4262c0800e7a45b
Kirill Golubev <arnasay@gmail.com>
2019-03-03 09:01:44 -0500

    Two new news sources added. Code refactored a bit

--- resources/main.js
+++ resources/main.js
@@ -6,9 +6,6 @@ const techcrunch = document.getElementById('techcrunch');
 // Add new news sources related objects
 const formObject = document.getElementById("sources");
 const sourcesList = document.getElementById('newSources');
-const sourcesMap = new Map();
-let newsCategories = [];
-const sourcesException = []; //variable used to track all news sources that don't contain proper content, but only description
 // Search related HTML objects
 const userSearch = document.getElementById('search');
 const searchButton = document.getElementById('searchButton');
@@ -27,9 +24,6 @@ const techcrunchURL = 'techcrunch';
 const initialize = () => {
   // Initialize news sources
   initializeNewsSources();
-  
-  // Add news sources exception that need special handling (in this case proper rendering of cyrillic letters)
-  sourcesException.push("rbc", "rt", "google-news-ru", "lenta");
 
   // Add event listeners
   bbc.addEventListener('click', (event) => { 
@@ -73,94 +67,7 @@ const initialize = () => {
 };
 
 // Function initializing map containing news sources and their urls, and adding them to html list
-const initializeNewsSources = () => {
-  // Map used to store urls for new news sources
-  sourcesMap.set("Select news", { category: '',
-                                  url: ''});
-  sourcesMap.set("AlJazeera", { category: 'World News',
-                                url: 'al-jazeera-english'});
-  sourcesMap.set("Google News Canada", { category: "World News",
-                                        url: 'google-news-ca'});
-  sourcesMap.set("Google News Russia", { category: "Russian News",
-                                        url: "google-news-ru"});
-  sourcesMap.set("New Scientist", { category: 'Technologies',
-                                    url: "new-scientist"});
-  sourcesMap.set("National Geographic", { category: 'Entertainment',
-                                          url: "national-geographic"});
-  sourcesMap.set("RBC", { category: "Russian News",
-                          url: "rbc"});
-  sourcesMap.set("The Telegraph", { category: 'World News',
-                                    url: "the-telegraph"});
-  sourcesMap.set("The Guardian", { category: 'World News',
-                                    url: "the-guardian-uk"});
-  sourcesMap.set("The Washington Post", { category: 'World News',
-                                          url: "the-washington-post"});
-  sourcesMap.set("Russia Today", { category: "Russian News",
-                                    url: "rt"});
-  sourcesMap.set("Wired", { category: 'Technologies',
-                            url: "wired"});
-  sourcesMap.set("The Huffington Post", { category: 'World News',
-                                          url: "the-huffington-post"});
-  sourcesMap.set("BBC Sport", { category: "Sport",
-                                url:"bbc-sport"});
-  sourcesMap.set("Next web", { category: 'Technologies',
-                                url: "the-next-web"});
-  sourcesMap.set("Recode", { category: 'Technologies',
-                              url: "recode"});
-  sourcesMap.set("Crypto News", { category: 'Technologies',
-                                  url: "crypto-coins-news"})
-  sourcesMap.set("Hacker News", { category: 'Technologies',
-                                  url: "hacker-news"});
-  sourcesMap.set("Buzzfeed", { category: 'Technologies',
-                                url: "buzzfeed"});
-  sourcesMap.set("Time", { category: 'World News',
-                          url: "time"});
-  sourcesMap.set("Financial Times", { category: 'Business News',
-                                      url:"financial-times"});
-  sourcesMap.set("Fox News", { category: "World News",
-                               url: "fox-news"});
-  sourcesMap.set("Independent", { category: "World News",
-                                  url: "independent"});
-  sourcesMap.set("Lenta", { category: "Russian News",
-                            url:"lenta"});
-  sourcesMap.set("Reddit /r/all",  { category: 'Entertainment',
-                                      url: "reddit-r-all"});
-  sourcesMap.set("The Economist", { category: 'Business News',
-                                    url: "the-economist"});
-  sourcesMap.set("USA Today", "usa-today");
-  sourcesMap.set("Ars Technica", { category: 'Technologies',
-                                    url: "ars-technica"});
-  sourcesMap.set("Entertainment Weekly", { category: 'Entertainment',
-                                            url: "entertainment-weekly"});
-  sourcesMap.set("Business Insider", { category: 'Business News',
-                                       url: "business-insider"});
-  sourcesMap.set("The New York Times", { category: 'World News',
-      url: "the-new-york-times"});
-  sourcesMap.set("The Wall Street Journal", { category: 'Business News',
-      url: "the-wall-street-journal"});
-  sourcesMap.set("The Globe and Mail", { category: 'World News',
-      url: "the-globe-and-mail"});
-  sourcesMap.set("New York Magazine", { category: 'World News',
-      url: "new-york-magazine"});
-  sourcesMap.set("Polygon", { category: 'Entertainment',
-      url: "polygon"});
-  sourcesMap.set("Next Big Future", { category: 'Technologies',
-      url: "next-big-future"});
-  sourcesMap.set("Mirror", { category: 'Entertainment',
-      url: "mirror"});
-  sourcesMap.set("The Sport Bible", { category: 'Sport',
-      url: "the-sport-bible"});
-  sourcesMap.set("ESPN", { category: 'Sport',
-      url: "espn"});
-  sourcesMap.set("TalkSport", { category: 'Sport',
-      url: "talksport"});
-  sourcesMap.set("Associated Press", { category: 'World News',
-      url: "associated-press"});
-  sourcesMap.set("RTE", { category: 'World News',
-      url: "rte"});
-
-  // Fill up categories array
-  newsCategories = ['', 'World News', 'Business News', 'Technologies', 'Entertainment', 'Russian News', 'Sport'];
+const initializeNewsSources = () => { 
 
   // Create and fill up dropdownlist with categories
   newsCategories.forEach(category => {
@@ -184,18 +91,17 @@ const initializeNewsSources = () => {
       } 
     }); 
 
-    //Append category to select HTML object
+    // Append category to select HTML object
     if (category !== '') {
       sourcesList.appendChild(categoryObject);
     }
-
   });
-
 };
 
-//News callback function used to load and render news from the url passed as a parameter
+// News callback function used to load and render news from the url passed as a parameter
 const addNewsSource = (sourceUrl) => {
   if (sourceUrl != "") {
+    // Add spinner html code
     main.innerHTML = '<div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
 
     getNews(sourceUrl).then(articlesArray => renderNews(articlesArray)).
@@ -205,7 +111,7 @@ const addNewsSource = (sourceUrl) => {
   }
 };
 
-//Method 
+// Method sending user seach request to News API
 const searchArticles = (userInput) => {
   if (userInput === "") {
     return;
@@ -228,7 +134,7 @@ const searchArticles = (userInput) => {
   }
 };
 
-//Request search results from news API 
+// Request search results from news API 
 const getSearchResults = async (userInput) => {
   try {
     //Clean the page and put spinner element
@@ -248,7 +154,7 @@ const getSearchResults = async (userInput) => {
 // Request News Function
 const getNews = async (url) => {
     try {
-        const response = await fetch(newsApiURL + url + apiKey + "&pageSize=90"); //Send asynchronous request to server 
+        const response = await fetch(newsApiURL + url + apiKey + "&pageSize=90"); //Send asynchronous request to server to get news search results
 
         if (response.ok) {
           const responseJson = await response.json();
@@ -298,9 +204,7 @@ function renderNews(articles) {
   return articles;
 }
 
-
-// Add functionality to social network buttons
-
+// Add some functionality to social network buttons
 function addSocialNetworksFunctionality(newsObjects) {
   let tweetButtons = document.getElementsByClassName('twitter');
   let facebookButtons = document.getElementsByClassName('facebook');
